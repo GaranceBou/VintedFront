aebb08465de145489be584eb5933434f0de2f89e
GaranceBou <garanceboussuge@gmail.com>
2024-03-27 18:21:49 +0100

    component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
-import logo from "./assets/logovinted.png";
-import banner from "./assets/banner.jpg";
-import Header from "./components/Header";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import {
   faMagnifyingGlass,
   faListAlt,
 } from "@fortawesome/free-solid-svg-icons";
 library.add(faMagnifyingGlass, faListAlt);
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Header from "./components/Header";
 
 function App() {
   const [data, setData] = useState([]);
@@ -28,57 +25,8 @@ function App() {
     fetchData();
   }, []);
 
-  return isLoading ? (
-    <span>En cours de chargement... </span>
-  ) : (
-    <header>
-      <section className="top">
-        <img src={logo} alt="logo" />
-        <div className="price">
-          <div className="bar">
-            <span>
-              <FontAwesomeIcon
-                icon="fa-solid fa-magnifying-glass"
-                style={{ color: "#bbbbbb" }}
-              />
-            </span>
-            <input
-              className="search"
-              type="text"
-              placeholder="Recherche des articles"
-            />
-          </div>
-          <div className="order">
-            <div className="set">
-              <p>Trier par prix :</p>
-              <input className="tickprice" type="checkbox" name="price" />
-            </div>
-            <div className="set">
-              <p>Prix entre :</p>
-            </div>
-          </div>
-        </div>
-        <div className="white">
-          <button>S'inscrire</button>
-          <button>Se connecter</button>
-        </div>
-        <button className="blue">Vends tes articles</button>
-      </section>
-      <section
-        className="hero"
-        style={{
-          backgroundImage: `url(${banner})`,
-        }}
-      >
-        <div className="replace">
-          <div className="rectangle">
-            <h2>Prêts à faire du tri dans vos placards ?</h2>
-            <button className="startselling">Commencer à vendre</button>
-          </div>
-        </div>
-      </section>
-    </header>
-  );
+  return isLoading ? <span>En cours de chargement... </span> : <Header />;
+
   // <div>
   //     {data.offers.map((offer, index) => {
   //       return (
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,57 @@
-const Header = () => {};
+import logo from "../assets/logovinted.png";
+import banner from "../assets/banner.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const Header = () => {
+  return (
+    <header>
+      <section className="top">
+        <img src={logo} alt="logo" />
+        <div className="price">
+          <div className="bar">
+            <span>
+              <FontAwesomeIcon
+                icon="fa-solid fa-magnifying-glass"
+                style={{ color: "#bbbbbb" }}
+              />
+            </span>
+            <input
+              className="search"
+              type="text"
+              placeholder="Recherche des articles"
+            />
+          </div>
+          <div className="order">
+            <div className="set">
+              <p>Trier par prix :</p>
+              <input className="tickprice" type="checkbox" name="price" />
+            </div>
+            <div className="set">
+              <p>Prix entre :</p>
+            </div>
+          </div>
+        </div>
+        <div className="white">
+          <button>S'inscrire</button>
+          <button>Se connecter</button>
+        </div>
+        <button className="blue">Vends tes articles</button>
+      </section>
+      <section
+        className="hero"
+        style={{
+          backgroundImage: `url(${banner})`,
+        }}
+      >
+        <div className="replace">
+          <div className="rectangle">
+            <h2>Prêts à faire du tri dans vos placards ?</h2>
+            <button className="startselling">Commencer à vendre</button>
+          </div>
+        </div>
+      </section>
+    </header>
+  );
+};
 
 export default Header;
