4bfbf28d461d4fd096c03e83ee8bf83d0db56490
GaranceBou <garanceboussuge@gmail.com>
2024-03-27 18:42:30 +0100

    offers filled

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import { useState, useEffect } from "react";
 import "./App.css";
-import axios from "axios";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import {
   faMagnifyingGlass,
@@ -12,19 +10,6 @@ import Home from "./components/Home";
 import Offers from "./components/Offers";
 
 function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await axios.get(
-        "https://lereacteur-vinted-api.herokuapp.com/offers"
-      );
-      setData(response.data);
-    };
-
-    fetchData();
-  }, []);
-
   return (
     <Router>
       <Routes>
@@ -33,16 +18,6 @@ function App() {
       </Routes>
     </Router>
   );
-
-  // <div>
-  //     {data.offers.map((offer, index) => {
-  //       return (
-  //         <div key={index}>
-  //           <p>{offer.product_description}</p>
-  //         </div>
-  //       );
-  //     })}
-  //   </div>
 }
 
 export default App;
--- src/components/Offers.jsx
+++ src/components/Offers.jsx
@@ -1,3 +1,37 @@
-const Offers = () => {};
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Header from "./Header";
+
+const Offers = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        "https://lereacteur-vinted-api.herokuapp.com/offers"
+      );
+      setData(response.data);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <section>
+      <Header />
+      <div>
+        {data.offers
+          ? data.offers.map((offer, index) => {
+              return (
+                <div key={index}>
+                  <p>{offer.product_name}</p>
+                </div>
+              );
+            })
+          : null}
+      </div>
+    </section>
+  );
+};
 
 export default Offers;
